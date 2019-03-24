aa9c510785bace1b13d0debfe393fd2d2abefe07
HBRYU <48374744+HBRYU@users.noreply.github.com>
2019-03-24 21:04:56 +0900

    fuck

---
+++ Flammable Blood/Assets/All/Scripts/CameraMan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMan : MonoBehaviour
+{
+    public float speed;
+    public GameObject target;
+    Vector3 movePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movePos = Vector3.Lerp(transform.position, target.transform.position, speed);
+        movePos.z = transform.position.z;
+        transform.position = movePos;
+    }
+}
