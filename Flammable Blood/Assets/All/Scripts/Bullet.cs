f1860dc1b1387fe2ef7314e07c86865bf18a323f
HBRYU <48374744+HBRYU@users.noreply.github.com>
2019-03-30 16:45:48 +0900

    업뎃

---
+++ Flammable Blood/Assets/All/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public GameObject wielder;
+    public float accuracy;
+    public float speed;
+    public float damage;
+
+    public float life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.Rotate(0, 0, Random.Range(-1, 1) * accuracy);
+        rb.velocity = transform.right * wielder.transform.localScale.x * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        life -= Time.deltaTime;
+        if(life <= 0) { Destroy(gameObject); }
+    }
+}
---
+++ Flammable Blood/Assets/All/Scripts/Gun_Rifle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun_Rifle : MonoBehaviour
+{
+    private GM _GM_;
+    private GameObject player;
+
+    public Bullet bullet;
+    public GameObject barrelEnd;
+    public float bulletSpeed;
+    public float fireRate;
+    public float accuracy;
+    public float damage;
+    public float magSize;
+    public float reloadSpeed;
+    
+    [HideInInspector]
+    public float fire_Timer, reload_Timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = _GM_.player;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fire_Timer += Time.deltaTime;
+        Debug.Log(fire_Timer);
+        if(fire_Timer >= fireRate)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Fire();
+                fire_Timer = 0;
+            }
+            
+        }
+    }
+
+    void Fire()
+    {
+        Debug.Log("FIRED");
+        Bullet thisBullet = bullet;
+        thisBullet.damage = damage;
+        thisBullet.accuracy = accuracy;
+        thisBullet.speed = bulletSpeed;
+        thisBullet.wielder = player;
+        Instantiate(thisBullet, barrelEnd.transform.position, Quaternion.identity);
+    }
+}
