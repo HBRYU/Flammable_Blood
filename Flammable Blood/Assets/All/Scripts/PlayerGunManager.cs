91ec3037d2af38aa9cb33a26f2a9647708278df6
HBRYU <48374744+HBRYU@users.noreply.github.com>
2019-03-31 11:33:02 +0900

    총 관리 스크립티 짜는 중

--- Flammable Blood/Assets/All/Scripts/PlayerGunManager.cs
+++ Flammable Blood/Assets/All/Scripts/PlayerGunManager.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class PlayerGunManager : MonoBehaviour
 {
-    public GameObject[] guns;
+    public int maxGunCount;
+    public List<GameObject> guns;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if(guns.Count > maxGunCount)
+        {
+            while(guns.Count > maxGunCount)
+            {
+                guns[guns.Count - 1].GetComponent<WeaponStats>().Drop();
+                guns.Remove(guns[guns.Count - 1]);
+            }
+        }
     }
 
     // Update is called once per frame
---
+++ Flammable Blood/Assets/All/Scripts/WeaponStats.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStats : MonoBehaviour
+{
+    public string name;
+    public int ID;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Drop()
+    {
+        transform.SetParent(null);
+        GetComponent<Rigidbody2D>().simulated = true;
+        GetComponent<PolygonCollider2D>().enabled = true;
+    }
+}
