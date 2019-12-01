85d85745fd64b102ee1750b340668fde10951350
HBRYU <48374744+HBRYU@users.noreply.github.com>
2019-12-01 22:07:52 +0900

    메인 메뉴 만드는 중

---
+++ Flammable Blood/Assets/All/Scripts/UI/TextAlternator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextAlternator : MonoBehaviour
+{
+    public List<string> texts;
+    public float interval;
+    private float interval_timer;
+
+    void Update()
+    {
+        interval_timer += Time.deltaTime;
+        if(interval_timer >= interval)
+        {
+            if (texts.IndexOf(GetComponent<TextMeshProUGUI>().text) + 1 < texts.Count)
+                GetComponent<TextMeshProUGUI>().text = texts[texts.IndexOf(GetComponent<TextMeshProUGUI>().text) + 1];
+            else
+                GetComponent<TextMeshProUGUI>().text = texts[0];
+            interval_timer = 0;
+        }
+    }
+}
